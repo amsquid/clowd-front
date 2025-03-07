68473faf64dbc7e6cdc48a0e0792bb9dc2a5e24f
Joseph Peterson <supercrazysquid@gmail.com>
2025-03-07 09:34:40 -0500

    Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 
+enum Menus {
+	FILE_EXPLORER,
+	TEXT_VIEWER,
+}
+
 const Dashboard = () => {
+	const [menu, setMenu] = useState(Menus.FILE_EXPLORER);
+
+	const menus = ["File Explorer"];
+
+	const handleClick = (item: string) => {
+		console.log(item);
+	};
+
 	return (
 		<div>
-			<Navbar />
+			<Navbar handleClick={handleClick} navBarItems={menus} />
 		</div>
 	);
 };
