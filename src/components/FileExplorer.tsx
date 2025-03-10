44054ef82db288fddb837ec99657eb228714e071
Joseph Peterson <supercrazysquid@gmail.com>
2025-03-10 16:05:29 -0400

    Basic frame for file explorer

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+import User from "../User";
+
 const FileExplorer = () => {
-	return <div>FileExplorer</div>;
+	const user: User = User.getInstance();
+
+	console.log(user.target);
+
+	return (
+		<div className="files">
+			<ul className="list-group">
+				<li className="list-group-item">An item</li>
+				<li className="list-group-item">A second item</li>
+				<li className="list-group-item">A third item</li>
+				<li className="list-group-item">A fourth item</li>
+				<li className="list-group-item">And a fifth one</li>
+			</ul>
+		</div>
+	);
 };
 
 export default FileExplorer;
