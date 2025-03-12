c32ec0209d7d55308907bf25649eef6be42d5323
Joseph Peterson <supercrazysquid@gmail.com>
2025-03-11 22:52:56 -0400

    Added username to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import User from "../User";
+
 interface Props {
 	handleClick(item: string): void;
 	navBarItems: string[];
@@ -5,12 +7,14 @@ interface Props {
 }
 
 const Navbar = ({ handleClick, navBarItems, selectedItem }: Props) => {
+	const user: User = User.getInstance();
+
 	return (
 		<div className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container-fluid">
 				<p className="navbar-brand">Clowd</p>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
+				<div className="collapse navbar-collapse" id="navbarText">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						{navBarItems.map((value: string, idx: number) => (
 							<a
 								className={
@@ -25,6 +29,7 @@ const Navbar = ({ handleClick, navBarItems, selectedItem }: Props) => {
 							</a>
 						))}
 					</ul>
+					<span className="navbar-text">{user.username}</span>
 				</div>
 			</div>
 		</div>
